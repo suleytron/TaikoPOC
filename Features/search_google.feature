40c3e39bcc97f5ae5bc27ee90fc45b8832f3430a
Suley Mahmoud <suleymahmoud@suleys-mbp.home>
2019-01-27 20:35:15 +0000

    testing out some packages

---
+++ Features/search_google.feature
@@ -0,0 +1,10 @@
+@complete
+Feature: run a google search
+  As a Searcher
+  I want to run a google search
+  So that I can try out these packages
+
+
+  Scenario: Adding a new SKU to the product database for review by a buyer
+    Given I have entered search on Google
+    And I feel lucky
---
+++ Features/step_definitions/search.js
@@ -0,0 +1,23 @@
+const driver = require('taiko');
+const { Given, Then, When, And, But , setDefaultTimeout} = require('cucumber');
+var randomWords = require('random-words');
+var Chance = require('chance');
+var chance = new Chance();
+var my_random_string = chance.string();
+
+
+setDefaultTimeout(60*1000);
+// Uncomment the appropriate line to run headless
+Given(/^I have entered search on Google$/, async function ()
+{
+    await driver.openBrowser({headless: false});
+    //await driver.openBrowser();
+    await driver.goto('http://www.google.com');
+   // await driver.write(randomWords(), driver.into(driver.textField()));
+    await driver.write(my_random_string, driver.into(driver.textField()));
+});
+
+
+    Given(/^I feel lucky$/, async function () {
+        await driver.click("I'm Feeling Lucky");
+    });
